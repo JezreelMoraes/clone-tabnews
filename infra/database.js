7bdf326c2bf7b877b479fc512f172a7ad6796edc
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2026-02-17 16:43:03 +0000

    Ativando conexao segura para banco

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ async function query(query, params) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
+    port: process.env.POSTGRES_PORT,
+    ssl: true
   })
 
   try {
