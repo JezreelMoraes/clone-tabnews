0e1505b6a91b04e9899033f6775977fc5a225312
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2026-02-17 15:18:05 +0000

    adicionando dados o /status e testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,26 @@
-import database from "infra/database.js";
+import database from 'infra/database';
 
 async function status(request, response) {
-  console.log(database);
-  const result = await database.query("SELECT 1+1 as sum;")
-  console.log(result.rows);
+  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+  
+  const maxConnectionsResult = await database.query("SHOW max_connections;")
+  const maxConnectionsValue = Number(maxConnectionsResult.rows[0].max_connections)
+  
+  const openedConnectionsResult = await database.query(
+    "SELECT count(*) FROM pg_catalog.pg_stat_activity WHERE state = 'active' AND datname = $1", [process.env.POSTGRES_DB]
+  )
+  const openedConnectionsValue = Number(openedConnectionsResult.rows[0].count)
 
   response.status(200).json({
-    chave: "retorno"
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnectionsValue,
+        opened_connections: openedConnectionsValue
+      }
+    }
   })
 }
 
--- tests/integration/status/get.test.js
+++ tests/integration/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json()
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+
+  const databaseDependency = responseBody.dependencies.database
+  expect(databaseDependency.version).toEqual("16.0")
+
+  const parsedMaxConnections = Number(databaseDependency.max_connections)
+  expect(databaseDependency.max_connections).toEqual(parsedMaxConnections)
+
+  const parsedOpenedConnections = Number(databaseDependency.opened_connections)
+  expect(databaseDependency.opened_connections).toEqual(parsedOpenedConnections)
+  expect(databaseDependency.opened_connections).toBe(1)
 })
